2fe4c34d3b783f077b2114f2ffe246e3976af2b2
chiamalite-codingal <chiamalitecodingal@gmail.com>
2024-09-21 17:39:51 +0100

    Divisible number

---
+++ main.py
@@ -0,0 +1,8 @@
+print("Enter numerator:")
+num = int(input())
+print("Enter denominator:")
+den = int(input())
+if num%den==0:
+    print(num,"is divisible by",den)
+else:
+    print("not divisible")
